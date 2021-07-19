e05ad4d724e47f554f72b8e6f6827f1ab8e73ccb
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-19 23:38:16 +0900

    feat: add async to postUpload

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -11,6 +11,7 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
     try{
         const videos = await Video.find({})
+        console.log(videos);
         return res.render("home", {pageTitle:"Home", videos});
     } catch {
         return res.render("server-error")
@@ -38,18 +39,19 @@ export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {title, description, hashtags} = req.body;
-    const video = new Video({
+    //const video = new Video({
+    await Video.create({
         title,
         description,
-        createAt: Date.now(),
+        createdAt: Date.now(),
         hashtags: hashtags.split(",").map(word => `#${word}`),
         meta: {
             views: 0,
             rating: 0,
         },
     })
-    console.log(video);
+    //await video.save();
     return res.redirect("/");
 }
