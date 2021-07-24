0ca9599cd61b5e81416c588ecc839522ce14bc23
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-24 17:33:43 +0900

    feat: locals를 통해서 로그인 유무를 template에 전달

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express"; 
 import morgan from "morgan";
 
+import session from "express-session";
+
 import rootRouter from "./router/rootRouter";
 import videoRouter from "./router/videoRouter";
 import userRouter from "./router/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const PORT = 4000
 
@@ -14,6 +17,14 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views")
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+}))
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter); 
