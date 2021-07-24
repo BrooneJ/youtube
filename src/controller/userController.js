70f469522f0aea9970ec8b720a8233e19c4509a4
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-25 00:17:34 +0900

    feat: add github Login

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -60,6 +60,38 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 }
 
+export const startGithubLogin = (req, res) => {
+    const baseUrl = `https://github.com/login/oauth/authorize`;
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: false,
+        scope: "read:user user:email",
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+
+export const finishGithubLogin = async(req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    }
+    console.log(config);
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const data = await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json", //이 부분을 넣지 않으면 json이 아니라 text가 됨
+        },
+    })
+    const json = await data.json();
+    console.log(json);
+}
+
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove");
 export const logout = (req, res) => res.send("logout");
