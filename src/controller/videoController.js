04f1c40959f1dec768a22ebc6dab739d09e7652e
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-27 21:16:44 +0900

    refator: use populate()

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -12,12 +12,12 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id); //id는 동영상의 id
-    const owner = await User.findById(video.owner); //video.owner의 id는 유저의 id
+    const video = await Video.findById(id).populate("owner");
+    console.log(video);
     if (!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
-    return res.render("watch", { pageTitle: video.title, video, owner });
+    return res.render("watch", { pageTitle: video.title, video });
 }
 
 export const getEdit = async (req, res) => {
