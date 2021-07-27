3b5d232b91bc2686e75c2a15127a0a6342054d8f
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-27 20:48:12 +0900

    feat: video에 user 정보 넣기

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async (req, res) => {
     try {
@@ -8,14 +9,17 @@ export const home = async (req, res) => {
         return res.render("server-error")
     }
 }
+
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
+    const video = await Video.findById(id); //id는 동영상의 id
+    const owner = await User.findById(video.owner); //video.owner의 id는 유저의 id
     if (!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
-    return res.render("watch", { pageTitle: video.title, video });
+    return res.render("watch", { pageTitle: video.title, video, owner });
 }
+
 export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
@@ -44,6 +48,7 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+    const { user: { _id } } = req.session;
     const { path: fileUrl } = req.file;
     const { title, description, hashtags } = req.body;
     //const video = new Video({
@@ -52,6 +57,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             fileUrl,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         })
         //await video.save();
